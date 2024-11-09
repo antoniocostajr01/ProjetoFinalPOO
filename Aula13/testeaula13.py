15f46957c33d78cd22f9dbc891ba1899d01bd2df
antoniocostajr01 <antonioclaudiocostajr@gmail.com>
2024-11-09 17:05:13 -0300

    .

--- Aula13/testeaula13.py
+++ Aula13/testeaula13.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog, ttk
+#Código feito pelo Ivonei para mostrar com funciona a interface gráfica do tkinter
+
 
 class Locker:
     def __init__(self, locker_id, locker_ocupado=False, locker_usuario=None):
